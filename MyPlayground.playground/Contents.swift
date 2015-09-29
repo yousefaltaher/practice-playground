b5d56f24da23e5672393fb3fa63f0ba1d92e3d8b
yousefaltaher <developer@Youseefs-MacBook-Air.local>
2015-09-29 21:14:56 +0200

    Revert "Revert "new steroids file""
    This reverts commit 61b036c60a1787953c681ff75b9ae2e1ed84585e.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var stri = "hey"
+
+
+
+var x : Int = 999
+
+
+var w = String(x)
+
+println(w)
+
+println(str)
+
